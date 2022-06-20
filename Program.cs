27b7473161a0094588158428a33293076448249a
Ignacio Garcia Vazquez <natxoga@gmail.com>
2022-06-20 21:04:35 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Console2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadLine();
+        }
+    }
+}
